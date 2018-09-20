fc59983769929764f9e11755c240e27491abbfee
Lucia Reynoso <rvaicul@gmail.com>
2018-09-20 15:50:00 -0700

    full functionality

--- Pop the Box/GameScene.swift
+++ Pop the Box/GameScene.swift
@@ -10,80 +10,100 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let squareSize = CGSize(width: 40, height: 40)
+    var score: Int = 0
+    var scoreLabel = SKLabelNode(fontNamed: "Helvetica")
+    var counter: Int = 0
+    enum GameState {
+        case run, gameOver
+    }
+    var state: GameState = .run
     
     override func didMove(to view: SKView) {
+        self.backgroundColor = .black
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        scoreLabel.text = "Score: \(score)"
+        scoreLabel.position = CGPoint(x: self.size.width / 2, y: self.size.height - 35)
+        scoreLabel.fontColor = .white
+        scoreLabel.fontSize = 30
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        addChild(scoreLabel)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+    func makeBox() {
+        let color = UIColor(hue: CGFloat.random(in: 0...1), saturation: 1, brightness: 1, alpha: 1)
+        let box = SKSpriteNode(texture: nil, color: color, size: squareSize)
+        box.name = "box"
+        let xPosition = Int.random(in: 20...Int(self.size.width - 20))
+        box.position = CGPoint(x: xPosition, y: 0)
+        addChild(box)
+        
+        // zoom!
+        let moveUp = SKAction.moveTo(y: size.height, duration: Double(Int.random(in: 1...6)))
+        let seq = SKAction.sequence([moveUp, .removeFromParent()])
+        box.run(seq) {
+            self.score -= 1
+            self.updateLabel()
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        if state == .gameOver {
+            if let view = self.view {
+                state = .run
+                // Load the SKScene from 'GameScene.sks'
+                let scene = GameScene(size: view.bounds.size)
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+                
+                view.ignoresSiblingOrder = true
+                
+                view.showsFPS = true
+                view.showsNodeCount = true
+            }
         }
+        if state == .run {
+            if let touch = touches.first {
+                let touchedNode = self.atPoint(touch.location(in: self))
+                if touchedNode.name == "box" {
+                    touchedNode.removeFromParent()
+                    score += 1
+                    updateLabel()
+                }
+            }
+        }
+    }
+    
+    func updateLabel() {
+        if score < 0 {
+            gameOver()
+        } else {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    func gameOver() {
+        state = .gameOver
+        self.removeAllChildren()
+        let gameOverLabel = SKLabelNode(fontNamed: "Helvetica")
+        gameOverLabel.text = "Game Over!"
+        gameOverLabel.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        gameOverLabel.fontColor = .white
+        gameOverLabel.fontSize = 45
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        addChild(gameOverLabel)
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if state != .run { return }
+        if counter <= 0 {
+            makeBox()
+            counter = 15 * Int.random(in: 1...4)
+        } else {
+            counter -= 1
+        }
     }
 }
